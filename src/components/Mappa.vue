7a411fee9a37a1f53f2b4085ab53c3cdc659a98e
VincentDS98 <disanto.vincenzo98@gmail.com>
2024-04-10 15:55:08 +0200

    fix show

---
+++ src/components/Mappa.vue
@@ -0,0 +1,56 @@
+
+
+<script>
+export default {
+  data() {
+    return {
+       // ID del container della mappa
+      center: [4, 44, 4], // Coordinate del centro della mappa
+      
+    };
+  },
+  mounted() {
+    //this.loadTomTomMapsScript();
+    this.initTomTomMap();
+  },
+  methods: {
+    loadTomTomMapsScript() {
+      // Crea un elemento script e aggiungilo al body del documento
+      const script = document.createElement('script');
+      script.src = 'https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps-web.min.js';
+
+      // Aggiungi un listener per gestire il caricamento dello script
+      script.onload = this.initTomTomMap;
+
+      // Aggiungi lo script al documento
+      document.body.appendChild(script);
+    },
+    initTomTomMap() {
+      // Inizializza la mappa TomTom
+      let center =  [4,44.4]
+      const map = tt.map({
+        key: 'x5vTIPGVXKGawffLrAoysmnVC9V0S8cq',
+        container: 'mapId',
+        center: center,
+        zoom: 10
+      });
+
+      // Aggiungi un marker al centro della mappa
+      map.on('load', () => {
+        new tt.Marker().setLngLat(center).addTo(map);
+      });
+    }
+  }
+};
+</script>
+
+
+<template>
+  <div id="mapId" style="width: 40vw; height: 350px;"></div>
+</template>
+
+<style scoped>
+/* Stili specifici del componente, se necessario */
+</style>
+
+  
--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -1,7 +1,10 @@
 <script >
 import axios from 'axios';
+
     export default {
-        data() {
+        data() 
+        {
+            
             return {
                 apartment: null,
                 name: '',
@@ -11,77 +14,90 @@ import axios from 'axios';
                 alert: false,
                 messageSent: false,
                 apartmentId: null,
-                showError: false
+                showError: false,
+                
             };
         },
+        created(){
+        this.getApartment();
+    },
+    /*mounted() {
+    //this.loadTomTomMapsScript();
+    this.initTomTomMap();
+  },*/
+    
+        
         methods: {
+            
+        
             
             getApartment() {
                 axios.get('http://127.0.0.1:8000/api/apartments/' + this.$route.params.slug)
                     .then(res => {
                         console.log(res.data);
 
-                        // Se la chiamata ha buon fine
-                        if (res.data.success == true) {
-                            // Si vedrà il singolo appartamento
-                            this.apartment = res.data.results;
-                            console.log(this.apartment);
-                            // Assegna l'ID dell'appartamento
-                            this.apartmentId = this.apartment.id; 
+                    // Se la chiamata ha buon fine
+                    if (res.data.success == true) {
+                        // Si vedrà il singolo appartamento
+                        this.apartment = res.data.results;
+                        console.log(this.apartment);
+                        // Assegna l'ID dell'appartamento
+                        this.apartmentId = this.apartment.id; 
 
-                        } else {
-                            // Altrimenti restituiamo la pagina d'errore
-                            this.$router.push({ name: 'not-found' });
-                        }
+                    } else {
+                        // Altrimenti restituiamo la pagina d'errore
+                        this.$router.push({ name: 'not-found' });
+                    }
 
-                    })
+                })
 
             },
             sendMessage() {
-                if (this.name != null
-                    && 
-                    this.name != ''
-                    &&
-                    this.name.length <= 64
-                    &&    
-                    this.email != null
-                    && 
-                    this.email != ''
-                    &&
-                    this.email.length <= 255
-                    &&
-                    this.message != null
-                    && 
-                    this.message != ''
-                    &&
-                    this.message.length <= 2048
-                    &&
-                    this.accepted 
-                ) {
+                    if (this.name != null
+                        && 
+                        this.name != ''
+                        &&
+                        this.name.length <= 64
+                        &&    
+                        this.email != null
+                        && 
+                        this.email != ''
+                        &&
+                        this.email.length <= 255
+                        &&
+                        this.message != null
+                        && 
+                        this.message != ''
+                        &&
+                        this.message.length <= 2048
+                        &&
+                        this.accepted 
+                    ){
                     axios.post('http://127.0.0.1:8000/api/contacts', {
-                            name: this.name,
-                            email: this.email,
-                            message: this.message,
-                            accepted: this.accepted,
-                            apartment_id: this.apartmentId
-                        })
-                         .then(res => {
-                            console.log('RISPOSTA API', res.data);
-                            console.log('ID APPARTAMENTO', this.apartment_id);
+                        name: this.name,
+                        email: this.email,
+                        message: this.message,
+                        accepted: this.accepted,
+                        apartment_id: this.apartmentId
+                    })
+                    .then(res => {
+                        console.log('RISPOSTA API', res.data);
+                        console.log('ID APPARTAMENTO', this.apartment_id);
 
-                            if (res.data.success) {
-                                this.messageSent = true;
-                                this.showError = false;
-                            }
-                        })
-                        .catch(err => {
-                            console.log('ERRORE AXIOS', err);
-                            this.alert = true;
-                            this.showError = true;
-                            this.alert = false
-                            //resetta il form
-                            this.resetForm();
-                        })
+                        if (res.data.success) {
+                            this.messageSent = true;
+                            this.showError = false;
+                        }
+                    })
+                    .catch(err => {
+                        console.log('ERRORE AXIOS', err);
+                        this.alert = true;
+                        this.showError = true;
+                        this.alert = false
+                        //resetta il form
+                        this.resetForm();
+                    })
+                        
                 }
                 else {
                     this.alert = true; 
@@ -96,17 +112,48 @@ import axios from 'axios';
                 this.messageSent = false;
                 this.apartmentId = null;
             },
-            
+           /* initTomTomMap() {
+                // Inizializza la mappa TomTom
+                let center =  [4,44.4]
+                const map = tt.map({
+                    key: '3lvx7A4Usgik9PGmpGZi4Q9yZlaw47Oi',
+                    container: 'mapId',
+                    center: center,
+                    zoom: 10
+                });
+
+             // Aggiungi un marker al centro della mappa
+             map.on('load', () => {
+                 new tt.Marker().setLngLat(center).addTo(map);
+            });
+        }*/
         },
-        created() {
-            this.getApartment();
+        computed: {
+            // Calcolo della parte intera del prezzo
+            priceInteger() {
+            return Math.floor(this.apartment.price);
+            },
+            // Calcolo della parte decimale del prezzo
+            priceDecimal() {
+            return (this.apartment.price % 1).toFixed(2).substring(1);
+            }
         }
-    }
+            
+    };
+    
+
 </script>
 
 <template>
-        <div class="d-flex col-12 justify-content-between ">
-            <div v-if="apartment != null" class="col-3 single-apartment ">
+    
+       <!-- <div id="mapId" style="width: 40vw; height: 350px;"></div>-->
+
+        <div class="row  justify-content-between ">
+
+           
+
+            <div v-if="apartment != null" class="col-sm-6  single-apartment ">
+                
 
                 <h2 class="text-center">
                     {{ apartment.title }}
@@ -126,18 +173,13 @@ import axios from 'axios';
                 </div>
                 <hr>
                 
-                <div v-for="sponsor in apartment.sponsors" :key="sponsor.id">
-                    <h5 class="my-3 text-success">
-                        Sponsor: {{ sponsor?.title ?? 'NULL' }}
-                        <!-- Sponsor: {{ sponsor ? sponsor.title : 'NULL' }} -->
-                    </h5>
-                </div>
-                <hr>
+              
                 <div class="row">
+                    <div>Servizzi:</div>
                     <div class="col-12">
-                        Servizi: 
+                         
                     <span v-if="apartment.services.length > 0">
-                        <span v-for="service in apartment.services" :key="service.id" class=" color badge rounded-pill ">
+                        <span v-for="service in apartment.services" :key="service.id" class=" color badge rounded-pill m-2 ">
                             <i :class="service.icon + ' pe-2'"></i> {{ service.title }}
                         </span>
                     </span>
@@ -146,13 +188,20 @@ import axios from 'axios';
                 </div>
                 <hr>
                 <div>
-                   <h4>Prezzo per notte {{ apartment.price }}</h4> 
+                   <p class="">
+                         
+                        <b> 
+                        <span  class="price-integer">{{ priceInteger }}</span>
+                        <span class="price-decimal ">{{ priceDecimal }}</span>
+                        </b>€/notte
+                    </p>  
                 </div>
+                
                 
 
             </div>     
         
-            <section class="col-4 justify-content-end">
+            <section class="col-sm-6  justify-content-end">
             <div v-if="!alert && !messageSent"  id="form-container" >
                 <h3>
                     Vuoi contattare il propretario dell'immobile?
@@ -225,21 +274,22 @@ import axios from 'axios';
     }
 }
 
+
 .color{
-    color: white;
-    background-color: $button_background_color;
+    color: black;
+    
 }
 
 section {
-    position: relative;
+    
 
     h3{
-        color: $secondary_text_color;
+        color: black;
         margin: 20px 0;
     }
 
     #form-container{
-        border: 1px solid coral;
+        border: 1px solid grey;
         padding: 10px;
         border-radius: 5%;
     }
@@ -248,25 +298,41 @@ section {
     form{
         
         label{
-            color: $primary_text_color;
+            color: black;
             &:last-child{
                 color: black;
             }
         }
         .submit_button{
-            background-color: $primary_background_color;
-            color: $secondary_text_color;
+            
+                border: 0;
+                padding: 7px 10px;
+                border-radius: 50px;
+                background-color: #ea4c89f8;
+                color: white;
+            
         }
         
     }
 
     .reset_button{
         
-        right: 0;
-        bottom: 0;
-        margin: 10px;
+        background-color: #cbcbcb;
+        border: 0.5px solid rgba(0, 0, 0, 0.521);
+        border-radius: 50px;
+        color: white;
+        padding: 7px 10px;
     }
+
+    
     
 }
+.price-integer {
+        font-size: 20px; 
+    }
+
+    .price-decimal {
+       font-size: 12px; 
+    }
 
 </style>
